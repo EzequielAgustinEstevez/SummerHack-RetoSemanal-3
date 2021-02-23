52ef18f07328c1c75d936efa20a0cdeae7ba0103
Ezequiel Agustin Estevez <ezecheze@gmail.com>
2021-02-23 19:47:02 -0300

    Desafio terminado

---
+++ img/categorias/Reto semanal #03_files/script.js
@@ -0,0 +1,18 @@
+
+let guardar = document.getElementById("guardar").addEventListener('click', function () {/* espera el click */
+    let producto = document.getElementById("Producto").value/* guardamos el valor del id Producto */
+    let nota = document.getElementById("Nota").value/* guardamos el valor del id Nota */
+    let categoria = document.getElementById("Categoria").value/* guardardamos el valor de Categoria */
+    if (producto !== '') {
+        document.getElementById("ListaProductosItems").innerHTML += `<li class="list-group-item"><img src="img/categorias/${categoria}.svg" alt="${producto}
+        class="listaProductosItems__icono">${producto}</li>`/* agregar item a la lista */
+        alert(producto)
+        producto, nota = ""
+        categoria = 0
+        document.getElementById("Producto").value = producto
+        document.getElementById("Nota").value = nota
+        alert(producto)
+    }
+
+
+})
--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,49 @@
-let categoria = document.getElementById("menusito")
-categoria.addEventListener('click', function () {
-    categoria = categoria.getAttribute("id")
+/* Guardar en lista desde modal */
+let guardar = document.getElementById("guardar").addEventListener('click', function () {/* espera el click */
+    let producto = document.getElementById("Producto").value/* guardamos el valor del id Producto */
+    let nota = document.getElementById("Nota").value/* guardamos el valor del id Nota */
+    let categoria = document.getElementById("Categoria").value/* guardardamos el valor de Categoria */
+    if (producto !== '') {
+        /* agregar item a la lista */
+        document.getElementById("ListaProductosItems").innerHTML += `<li class="list-group-item" data-producto="${producto}" data-nota="${nota}" data-categoria="${categoria}"><img src="img/categorias/${categoria}.svg" alt="${producto}
+        class="listaProductosItems__icono">${producto}</li>`
+        /* Limpiar campos */
+        document.getElementById("Producto").value = ""
+        document.getElementById("Nota").value = ""
+        document.getElementById("Categoria").value = "Carnes"
+        document.getElementById("ListaProductos").style.display = "block"
+        document.getElementById("ListaVacia").style.display = "none"
+        document.getElementById("BorrarTodo").style.display = "block"
+    }
 })
-alert(categoria)
-let guardar = document.getElementById('guardar').addEventListener('click', function () {
-    let producto = document.getElementById("Producto").value
-    let nota = document.getElementById("Nota").value
+/* Mostrar detalle de productos */
+let mas = document.getElementById("ListaProductosItems").addEventListener('click', function (e) {
+    document.getElementById("ListaProductos").style.display = "none"
+    document.getElementById("DetalleProducto").style.display = "flex"
+    
+    let producto = e.target.getAttribute('data-producto')
+    let categoria = e.target.getAttribute('data-categoria')
+    let nota = e.target.getAttribute('data-nota')
+    document.getElementById("volver__button").style.display = "block"
+    document.getElementById("DetalleProducto").innerHTML = `<img src="/img/categorias/${categoria}.svg" alt="${categoria}">
+    <h1 class="mt-3">${producto}</h1>
+    <p class="mt-2">${nota}</p>`
+    
+})
 
+/* Mostrar lista de productos */
+let menos = document.getElementById("volver__button").addEventListener('click', function () {
+    document.getElementById("ListaProductos").style.display = "block"
+    document.getElementById("DetalleProducto").style.display = "none"
+    document.getElementById("volver__button").style.display = "none"
+    document.getElementById("BorrarTodo").style.display = "block"
+    
+})
 
-
-    console.log(`Nombre del producto: ${producto}, Categoria: ${categoria}, Nota: ${nota}`)
+/* Borrar todo */
+document.getElementById("BorrarTodo").addEventListener('click', function () {
+    document.getElementById("ListaProductosItems").innerHTML = ``
+    document.getElementById("ListaProductos").style.display = "none"
+    document.getElementById("ListaVacia").style.display = "flex"
+    document.getElementById("BorrarTodo").style.display = "none"
 })
